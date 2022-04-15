8eae5d18594241e9ec77ef0e0efe5dc04aa64f49
labbomb <739955946@qq.com>
2022-04-15 10:58:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ async function updateTopTracks(json) {
     artist = truncate(artist, 19)
 
     const line = [
-      name.padEnd(25 + name.length - eaw.length(name)),
-      artist.padStart(19 + artist.length - eaw.length(artist)),
+      name.padEnd(30 + name.length - eaw.length(name)),
+      artist.padStart(20 + artist.length - eaw.length(artist)),
     ]
     lines.push(line.join(''))
   }
