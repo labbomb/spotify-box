184307bd40f8b95ae2588b035cb579b17bea59a3
labbomb <739955946@qq.com>
2022-04-16 02:54:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ async function main() {
   await updateTopTracks(json)
 }
 
+function trimRightStr(str, len) {
+  // Ellipsis takes 3 positions, so the index of substring is 0 to total length - 3.
+  return str.length > len ? str.substring(0, len - 3) + "..." : str;
+}
+
 async function updateTopTracks(json) {
   let gist
   try {
@@ -42,8 +47,11 @@ async function updateTopTracks(json) {
     artist = truncate(artist, 19)
 
     const line = [
-      name.padEnd(31 + name.length - eaw.length(name)),
-      artist.padStart(20 + artist.length - eaw.length(artist)),
+      trimRightStr(name, 20).padEnd(10),
+      generateBarChart(100, 15),
+      artist.padStart(5)
+//       name.padEnd(31 + name.length - eaw.length(name)),
+//       artist.padStart(20 + artist.length - eaw.length(artist)),
     ]
     lines.push(line.join(''))
   }
@@ -64,6 +72,22 @@ async function updateTopTracks(json) {
       `spotify-box ran into an issue for updating your gist:\n${error}`
     )
   }
+}
+
+function generateBarChart(percent, size) {
+//   const syms = "░▏▎▍▌▋▊▉█";
+  const syms = "         ";
+
+  const frac = Math.floor((size * 8 * percent) / 100);
+  const barsFull = Math.floor(frac / 8);
+  if (barsFull >= size) {
+    return syms.substring(8, 9).repeat(size);
+  }
+  const semi = frac % 8;
+
+  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+    .join("")
+    .padEnd(size, syms.substring(0, 1));
 }
 
 function truncate(str, len) {
